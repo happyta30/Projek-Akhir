fdd1060ce2c0777bc3ccd210edd190572fd1045d
adeirvan123 <adenganjuk55@gmail.com>
2022-06-06 22:45:03 +0700

    update again

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
-import numpy as np
+# import numpy as np
 import joblib
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -9,19 +10,20 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/masterpeta') 
+@app.route('/masterpeta', methods=['GET','POST']) 
 def masterpeta():
     return render_template('masterpeta.html',menu='master', submenu='peta')
 
-@app.route('/masterprediksi') 
+
+@app.route('/masterprediksi', methods=['GET','POST']) 
 def masterprediksi():
     if request.method == 'GET':
         print(request.form)
         return render_template('masterprediksi.html')
     elif request.method == 'POST':
          # Get values through input bars
-        model = joblib.load("../model_development/model_predict_home.pkl")
-        income = request.form.get("height")
+        model, std_scaler  = joblib.load("model_development/model_predict_home.pkl")
+        income = request.form.get("income")
         age = request.form.get("age")
         bathroom = request.form.get("bathroom")
         bedroom = request.form.get("bedroom")
@@ -31,32 +33,34 @@ def masterprediksi():
         X = pd.DataFrame([[income, age, bathroom, bedroom, population]], columns = ["income", "age", "bathroom", "bedroom", "population"])
         
         # Get prediction
-        predict = model.predict(X)[0]
-        
+        predict_price = round(model.predict(X)[0],2)      
     else:
-        predict = ""
-        
-    return render_template("masterprediksi.html", output = predict)
-           
-        
-    #     print(dict(request.form))
-    #     X_feature = dict(request.form).values()
-    #     X_feature = np.array([float(x) for x in X_feature])
-        
-    #     X_feature = X_feature.reshape(1,-1) # satu baris unknown columns[1,2,3,4,]
-    #     print(X_feature)
-    #     result = model.predict(X_feature)
-    #     # edit hasil disini
-    #     # result = bla bla bla
-    #     return render_template('masterprediksi.html', result=result )
-    #      # prediction_text="The house should be sold for $ {}".format(output)
-    # else:
-    #     return "Unsupported Request Method"
+        predict_price = ""    
+    return render_template("masterprediksi.html", output = predict_price)
 
 
-@app.route('/masterprediksiusia') 
+@app.route('/masterprediksiusia', methods=['GET','POST']) 
 def masterprediksiusia():
-    return render_template('masterprediksiusia.html',menu='master', submenu='prediksi')
+    if request.method == 'GET':
+        print(request.form)
+        return render_template('masterprediksiusia.html')
+    elif request.method == 'POST':
+         # Get values through input bars
+        model2, std_scaler2  = joblib.load("model_development/model_predict_home_age.pkl")
+        income = request.form.get("income")
+        bathroom = request.form.get("bathroom")
+        bedroom = request.form.get("bedroom")
+        population = request.form.get("population")
+        price = request.form.get("price")
+        
+        # Put inputs to dataframe
+        X = pd.DataFrame([[income, bathroom, bedroom, population, price]], columns = ["income", "bathroom", "bedroom", "population", "price"])
+        
+        # Get prediction
+        predict_age = round(model2.predict(X)[0])      
+    else:
+        predict_age = ""   
+    return render_template("masterprediksiusia.html", output_age = predict_age)
 
 
 if __name__ == "__main__":
